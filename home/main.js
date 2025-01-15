18c6352fb73da6622d20f657af8ccfb83d0861c1
LukeGeyer1117 <luke.geyer@utahtech.edu>
2025-01-15 13:49:40 -0700

    made some pretty changes and added video embed

---
+++ home/main.js
@@ -0,0 +1,8 @@
+let monthButton = document.querySelector("#month-dropdown-button");
+let dropdownList = document.querySelector("#month-dropdown-menu");
+
+monthButton.addEventListener('click', (event) => {
+    console.log(monthButton);
+    console.log(dropdownList);
+    dropdownList.style.visibility  
+})
